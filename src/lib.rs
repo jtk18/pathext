fa5d60b32c4541a4100406a8a2107a63ba4fecc1
Jason Kiniry <jason.kiniry@gmail.com>
2021-12-30 17:44:26 -0600

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,29 @@ pub trait PathExt {
 
 impl<T: AsRef<Path>> PathExt for T {
     fn contains_or_ends_with(&self, pattern: &str) -> bool {
-        self.as_ref().to_str().map(|s| s.contains(pattern) || s.ends_with(pattern)) == Some(true)
+        self.as_ref()
+            .to_str()
+            .map(|s| s.contains(pattern) || s.ends_with(pattern))
+            == Some(true)
     }
 
     fn contains_or_starts_with(&self, pattern: &str) -> bool {
-        self.as_ref().to_str().map(|s| s.contains(pattern) || s.starts_with(pattern)) == Some(true)
+        self.as_ref()
+            .to_str()
+            .map(|s| s.contains(pattern) || s.starts_with(pattern))
+            == Some(true)
     }
 
     fn starts_or_ends_with(&self, pattern: &str) -> bool {
-        self.as_ref().to_str().map(|s| s.starts_with(pattern) || s.ends_with(pattern)) == Some(true)
+        self.as_ref()
+            .to_str()
+            .map(|s| s.starts_with(pattern) || s.ends_with(pattern))
+            == Some(true)
     }
 
     fn has_component(&self, component: &str) -> bool {
-        self.as_ref().components().any(|c| c.as_os_str().eq(component))
+        self.as_ref()
+            .components()
+            .any(|c| c.as_os_str().eq(component))
     }
 }
